1e17666a92658c4b7c29fc329c13864cad6a3883
ruuffian <ruuffian@gmail.com>
2024-08-31 20:22:45 -0400

    Added better error handling

--- src/main/kotlin/RabbitMQBridge.kt
+++ src/main/kotlin/RabbitMQBridge.kt
@@ -2,8 +2,8 @@ package com.lowbudgetlcs
 
 import com.lowbudgetlcs.data.Result
 import com.rabbitmq.client.*
-import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import org.slf4j.LoggerFactory
@@ -38,18 +38,23 @@ class RabbitMQBridge(private val exchangeName: String = "RIOT_CALLBACKS") {
             val callback = DeliverCallback { _, delivery: Delivery ->
                 val topic = delivery.envelope.routingKey
                 logger.info("[x] Recieved data on $topic topic.")
+                val message = String(delivery.body, charset("UTF-8"))
                 when (topic) {
                     "callback" -> {
-                        val message = String(delivery.body, charset("UTF-8"))
-                        val result = Json.decodeFromString<Result>(message)
-                        logger.debug("Callback: {}", result.toString())
-                        // Actually do something with the callback
-                        StatsHandler(result).receiveCallback()
+                        try {
+                            val result = Json.decodeFromString<Result>(message)
+                            logger.debug("Callback: {}", result.toString())
+                            StatsHandler(result).receiveCallback()
+                        } catch (e: SerializationException) {
+                            logger.error("SerializationException occured while deserializing message.")
+                        } catch (e: IllegalArgumentException) {
+                            logger.warn("Illegal message body recieved: {}", message)
+                        }
                     }
 
                     "series" -> TODO()
                     else -> {
-                        logger.info("Recieved unkown topic: '{}'.", topic)
+                        logger.warn("Recieved unkown topic: '{}'.", topic)
                     }
                 }
             }
